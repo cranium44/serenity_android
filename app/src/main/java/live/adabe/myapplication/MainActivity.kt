cee56e2bb24e3de256ed5681072a72ad79e1126c
Adabe Echaim <adabeechaim@gmail.com>
2022-04-04 19:47:45 +0100

    implemented reading audio

--- app/src/main/java/live/adabe/myapplication/MainActivity.kt
+++ app/src/main/java/live/adabe/myapplication/MainActivity.kt
@@ -1,15 +1,21 @@
 package live.adabe.myapplication
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.media.MediaDataSource
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
 import android.util.Log
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.recyclerview.widget.LinearLayoutManager
 import dagger.hilt.android.AndroidEntryPoint
 import live.adabe.myapplication.databinding.ActivityMainBinding
 import live.adabe.myapplication.feature_audio.models.MusicObject
 import live.adabe.myapplication.feature_audio.ui.MusicListAdapter
 import java.util.concurrent.TimeUnit
-
 
 //@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -21,19 +27,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        getAllMusicFiles()
-    }
-
-    override fun onStart() {
-        super.onStart()
-        getAllMusicFiles()
+        songsAdapter = MusicListAdapter(getAllMusicFiles(), listener)
+        initviews()
     }
 
     private fun initviews() {
-        binding.apply { }
+        binding.apply {
+            musicListRv.adapter = songsAdapter
+            musicListRv.layoutManager = LinearLayoutManager(this@MainActivity)
+        }
     }
 
-    private fun getAllMusicFiles() {
+    private fun getAllMusicFiles(): List<MusicObject> {
         val tempSongs = mutableListOf<MusicObject>()
         val uri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI
         val projection = arrayOf(
@@ -41,6 +46,7 @@ class MainActivity : AppCompatActivity() {
             MediaStore.Audio.AudioColumns.TITLE,
             MediaStore.Audio.AudioColumns.DATA,
             MediaStore.Audio.AudioColumns.ARTIST,
+            MediaStore.Audio.AudioColumns.DISPLAY_NAME
         )
         val selection = "${MediaStore.Audio.AudioColumns.DURATION} >= ?"
         val selectionArgs = arrayOf(TimeUnit.MILLISECONDS.convert(30L, TimeUnit.SECONDS).toString())
@@ -59,12 +65,45 @@ class MainActivity : AppCompatActivity() {
                 val name = it1.getString(1)
                 val path = it1.getString(2)
                 val artist = it1.getString(3)
-                
-                tempSongs.add(MusicObject(id, name, path, artist))
+                val displayName = it1.getColumnName(4)
+
+                tempSongs.add(MusicObject(id, name, path, artist, displayName))
             }
             it1.close()
         }
 
         Log.d("MAIN_ACTIVITY", tempSongs.joinToString { it.toString() } + "file")
+
+        return tempSongs
+    }
+
+    private fun getPermissions() {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            ) != PackageManager.PERMISSION_DENIED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), 100
+            )
+        }
+    }
+
+    private val listener = object : MusicListAdapter.OnMusicItemClickListener {
+        override fun onItemClick(musicObject: MusicObject) {
+            Toast.makeText(this@MainActivity, "Artist ${musicObject.artist}", Toast.LENGTH_SHORT).show()
+
+            try{
+                val player = MediaPlayer()
+
+                player.setDataSource(musicObject.path)
+                player.prepare()
+                player.start()
+
+            }catch (t: Throwable){
+                Toast.makeText(this@MainActivity, "Error Playing Audio", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
--- app/src/main/java/live/adabe/myapplication/feature_audio/models/MusicObject.kt
+++ app/src/main/java/live/adabe/myapplication/feature_audio/models/MusicObject.kt
@@ -5,4 +5,5 @@ data class MusicObject(
     val name: String,
     val path: String,
     val artist: String,
+    val displayName: String,
 )
