276f391f29aa7cf4526106cce6a0b7a736d2d725
Adabe Echaim <adabeechaim@gmail.com>
2022-04-04 20:55:18 +0100

    add utility function to add effect to views

---
+++ app/src/main/java/live/adabe/myapplication/utils/Extensions.kt
@@ -0,0 +1,23 @@
+package live.adabe.myapplication.utils
+
+import android.annotation.SuppressLint
+import android.graphics.PorterDuff
+import android.view.MotionEvent
+import android.view.View
+
+@SuppressLint("ClickableViewAccessibility")
+fun buttonEffect(button: View) {
+    button.setOnTouchListener { v, event ->
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+                v.background.setColorFilter(-0x1f0b8adf, PorterDuff.Mode.SRC_ATOP)
+                v.invalidate()
+            }
+            MotionEvent.ACTION_UP -> {
+                v.background.clearColorFilter()
+                v.invalidate()
+            }
+        }
+        false
+    }
+}
